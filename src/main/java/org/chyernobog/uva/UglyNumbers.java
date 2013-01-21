6d233fc951e20a26b1c8d38431eac06d9a22856a
Roger Turnau <rturnau@gmail.com>
2013-01-20 20:29:27 -0500

    first bash at problem.

--- src/main/java/org/chyernobog/uva/UglyNumbers.java
+++ src/main/java/org/chyernobog/uva/UglyNumbers.java
@@ -16,93 +16,59 @@
  */
 package org.chyernobog.uva;
 
-import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Queue;
+import java.util.Set;
 
 /**
- * There are two ways to do this one: 
- * 1) create a list of numbers whose prime factorization consists only of 2, 3, or 5
- *    this requires writing a prime factorization routine
- * 2) Just print out the 1500th number to stdout. Do the calculations in Haskell. (heh, heh...)
- * ... but, since my goal is to learn these algorithms in Java, I'll stick to Java
- * 
- * gotchas here are whether or not we need BigInt. 
+ * There are two ways to do this one: 1) Generate a huge list of numbers, and
+ * try to determine which ones have a prime factorization of only 2, 3, or 5 2)
+ * Generate the list of ugly numbers by an ultra-simple BFS. multiply 1 by 2,
+ * then 3, then 5. Then the challenge becomes sorting. 3) Just print out the
+ * 1500th number to stdout. Do the calculations in Haskell. (heh, heh...) ...
+ * but, since my goal is to learn these algorithms in Java, I'll stick to Java
+ *
  * @author turnau_r
  */
 public class UglyNumbers {
-    
-    public static final BigInteger ZERO = BigInteger.ZERO;
-    public static final BigInteger ONE = BigInteger.ONE;
-    public static final BigInteger TWO = BigInteger.valueOf(2);
-    public static final int[] aValues = {2,3,5,7,11,13,17,19,23,29,31,37,41}; // values for bases
-    
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-//        List<Long> uglyNums = new ArrayList<Long>();
-        // "By convention, 1 is included."
-//        uglyNums.add(1L);
-//        
-//        List<Long> factors = new ArrayList<Long>();
-        
-        // get prime factors for an integer using Pollard's rho algorithm
-        
-        long begin = System.currentTimeMillis();
-        
-        BigInteger n = new BigInteger("44343535354351600000003434353");
-        boolean ret = millerRabin(n, 13);
-        long end = System.currentTimeMillis();
-        System.out.println(ret);
-        System.out.println("time: " + (end - begin));
-        
-        System.out.println("");
-        
-        
-    }
-    
-    public static List<Long> factor(long n) {
-        return null;
-    }
-    
-    /**
-     * Miller Rabin primality test
-     * @param n
-     * @param numValues
-     * @return 
-     */
-    public static boolean millerRabin(BigInteger n, int numValues) { // n = number to test
-        BigInteger d = n.subtract(ONE);                              // numValues = # of bases to test
-        int s = 0;
-        while(d.mod(TWO).equals(ZERO)){
-            s++;
-            d = d.divide(TWO);
-        }
-        System.out.print("Base ");
-        for (int i = 0; i < numValues; i++) {
-            BigInteger a = BigInteger.valueOf(aValues[i]);  // composite
-            boolean r = testPr(n, a, s, d);
-            System.out.println(aValues[i] + " ");
-            if (!r) {
-                return false;
-            }
-        }
-        return true;
-    }
-     
-    private static boolean testPr(BigInteger n, BigInteger a, int s, BigInteger d) {
-        for (int i = 0; i < s; i++) {
-            BigInteger exp = TWO.pow(i);
-            exp = exp.multiply(d);
-            BigInteger res = a.modPow(exp, n);
-            if (res.equals(n.subtract(ONE)) || res.equals(ONE)) {
-                return true;
-            }
-        }
-        return false;
-    }
-     
 
-    
+    public static void main(String[] args) {
+        Set<Long> uglies = new HashSet<Long>();
+        long next = 0L;
+
+        // do a simple BFS -- put first elt on a queue
+        Queue<Long> ugQueue = new ArrayDeque<Long>();
+
+        ugQueue.add(1L);
+
+        while (!ugQueue.isEmpty() && uglies.size() < 1500) {
+            next = ugQueue.poll();
+            uglies.add(next);
+            if (!ugQueue.contains(next * 2L)) {
+                ugQueue.add(next * 2L);
+            }
+            if (!ugQueue.contains(next * 3L)) {
+                ugQueue.add(next * 3L);
+            }
+            
+            if (!ugQueue.contains(next * 5L)) {
+                ugQueue.add(next * 5L);
+            }
+        }
+
+
+
+        Long[] ugs = uglies.toArray(new Long[0]);
+
+        Arrays.sort(ugs);
+
+//        for (int x = 0; x < ugs.length; x++) {
+//            System.out.println("The " + (x + 1) + " 'th ugly number is " + ugs[x] + ".");
+//        }
+
+        System.out.println("The 1500'th ugly number is " + ugs[1499] + ".");
+
+    }
 }
