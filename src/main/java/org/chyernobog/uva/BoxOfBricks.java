0f235eecfdc276adeaa936794964b0e4a079cd22
Roger Turnau <rturnau@gmail.com>
2013-03-10 17:17:19 -0400

    got AC on BoxOfBricks

--- src/main/java/org/chyernobog/uva/BoxOfBricks.java
+++ src/main/java/org/chyernobog/uva/BoxOfBricks.java
@@ -25,23 +25,35 @@ import java.util.Scanner;
 public class BoxOfBricks {
     
     public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-        Scanner sc = new Scanner("6\n5 2 4 1 7 5\n0");    
-        int blks = 0;
+        Scanner sc = new Scanner(System.in);
+//        Scanner sc = new Scanner("6\n5 2 4 1 7 5\n3\n7 1 1\n0");    
+        int blks;
+        int set = 1;
         while ((blks = sc.nextInt()) != 0) {
             int first = sc.nextInt();
             int[] st = new int[blks];
             st[0] = first;
             int height = first;
-            System.out.println("st[" + 0 + "]: " + st[0]);
             for (int x = 1; x < blks; x++) {
                 st[x] = sc.nextInt();
                 height += st[x];
-                System.out.println("st[" + x + "]: " + st[x]);
-                
             }
-            System.out.println("height:" + height/blks);
-            first = 0;
+            int level = height/blks;
+            int totmoves = 0;
+            for (int x = 0; x < blks; x++) {
+                if (st[x] < level) {
+                    totmoves += level - st[x];
+                }
+            }
+            System.out.println("Set #" + set++);
+            System.out.println("The minimum number of moves is " + totmoves + "." );
+            System.out.println();
+            blks = 0;
+            first = 0; 
+            height = 0;
+            level = 0;
+            totmoves = 0;
+            st = null;
         }
     }
     
