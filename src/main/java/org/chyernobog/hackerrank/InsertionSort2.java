ddb0f3d39f7c2798907d94b4e00796cc8f367274
Roger Turnau <rturnau@gmail.com>
2013-05-12 22:09:18 -0400

    implemented challenge2

--- src/main/java/org/chyernobog/hackerrank/InsertionSort2.java
+++ src/main/java/org/chyernobog/hackerrank/InsertionSort2.java
@@ -16,10 +16,57 @@
  */
 package org.chyernobog.hackerrank;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  *
  * @author roger
  */
 public class InsertionSort2 {
-    
+
+    public static void insertionSort(int[] ar) {
+        if (ar.length > 1) {
+            for (int x = 1; x <= ar.length; x++) {
+                int v = ar[x - 1];
+                for (int i = x-2; i >= 0; i--) {
+                    if (v < ar[i]) {
+                        ar[i + 1] = ar[i];
+                        if (i == 0) {
+                            ar[i] = v;
+                            printArray(ar);
+                        }
+                    } else {
+                        ar[i + 1] = v;
+                        printArray(ar);
+                        break;
+                    }
+                }
+            }
+        }
+
+
+    }
+    /* Tail starts here */
+
+    static void printArray(int[] ar) {
+        for (int n : ar) {
+            System.out.print(n + " ");
+        }
+        System.out.println("");
+    }
+
+    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        int n = in.nextInt();
+//        int[] ar = new int[n];
+//        for (int i = 0; i < n; i++) {
+//            ar[i] = in.nextInt();
+//        }
+//        int[] ar = {1};
+//        int[] ar = {1, 2, 3, 4, 5, 6};
+//        int[] ar = {};
+        int[] ar = {1, 4, 3, 5, 6, 2};
+        insertionSort(ar);
+    }
 }
